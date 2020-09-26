a7e77239435017fc36f2b45eba450788e9a066b4
mbabali iryn <mbabaliiryn@gmail.com>
2020-09-26 15:29:29 +0300

    create loadingPage component

---
+++ src/components/loadingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/loading.scss';
+
+const LoadingPage = () => (
+  <div className="loading-section">
+    <div className="loader" />
+  </div>
+
+);
+
+export default LoadingPage;
